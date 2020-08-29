041c0bfe2b909fb35bfacfa6ce4fd5b380472956
Vlad Sirbu <sirbuvlad.com@gmail.com>
2020-08-29 11:32:14 +0200

    Updates

---
+++ precache-manifest.d2510d187b23fd1caaec85ddaf1df77d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d3bee7c9b8f78b2aea7fd8e0d91877",
+    "url": "/react-monsters-app/index.html"
+  },
+  {
+    "revision": "599d571589635d844003",
+    "url": "/react-monsters-app/static/css/main.0c1a194e.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/react-monsters-app/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters-app/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "599d571589635d844003",
+    "url": "/react-monsters-app/static/js/main.d16c9aaf.chunk.js"
+  },
+  {
+    "revision": "14215bf600b28bffad29",
+    "url": "/react-monsters-app/static/js/runtime-main.bd0180e3.js"
+  }
+]);
